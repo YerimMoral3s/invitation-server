db9c2515b1ef83dc4f6940afbb4ab125377cfb06
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-06-16 10:16:43 -0600

    test

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,12 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'https://yerimmoral3s.github.io/Inivitacion', "https://yerimmoral3s.github.io", "*"]
+    }
+  },
 ];
