00eef0260d4f3c1984317481597f77b4cd53d8d1
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-11-21 23:08:41 -0600

    add findWithSubGuests & guests-with-sub-guests

--- src/api/guest/controllers/guest.ts
+++ src/api/guest/controllers/guest.ts
@@ -2,6 +2,47 @@
  * guest controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::guest.guest');
+export default factories.createCoreController(
+  "api::guest.guest",
+  ({ strapi }) => ({
+    async findWithSubGuests(ctx) {
+      try {
+        const data = await strapi.db.query("api::guest.guest").findMany({
+          populate: ["sub_guests"],
+        });
+
+        const formattedData = data.map((guest) => ({
+          id: guest.id,
+          attributes: {
+            phone_number: guest.phone_number,
+            name: guest.name,
+            createdAt: guest.createdAt,
+            updatedAt: guest.updatedAt,
+            publishedAt: guest.publishedAt,
+            civil_confirmation: guest.civil_confirmation,
+            religious_confirmation: guest.religious_confirmation,
+            seen: guest.seen,
+            sub_guests: {
+              data: guest.sub_guests.map((subGuest) => ({
+                id: subGuest.id,
+                attributes: {
+                  name: subGuest.name,
+                  confirmation: subGuest.confirmation,
+                  createdAt: subGuest.createdAt,
+                  updatedAt: subGuest.updatedAt,
+                  publishedAt: subGuest.publishedAt,
+                },
+              })),
+            },
+          },
+        }));
+
+        ctx.body = { data: formattedData };
+      } catch (error) {
+        ctx.throw(500, "Error fetching guests with sub-guests");
+      }
+    },
+  })
+);
--- src/api/guest/routes/guest.ts
+++ src/api/guest/routes/guest.ts
@@ -2,6 +2,28 @@
  * guest router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
+const defaultRouter = factories.createCoreRouter("api::guest.guest");
 
-export default factories.createCoreRouter('api::guest.guest');
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const extraRoutes = [
+  {
+    method: "GET",
+    path: "/guests-with-sub-guests", // Ruta personalizada
+    handler: "guest.findWithSubGuests", // Controlador y método
+  },
+];
+
+module.exports = customRouter(defaultRouter, extraRoutes);
